e392557a0428f3fd126f069f1451390534b77f30
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-03-27 11:54:24 +0100

    Update NavBarWebTest to solve a problem with UsuarioListWebTest

--- src/test/java/todolist/controller/NavbarWebTest.java
+++ src/test/java/todolist/controller/NavbarWebTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import todolist.authentication.ManagerUserSession;
 import todolist.dto.UsuarioData;
@@ -12,11 +13,13 @@ import todolist.service.UsuarioService;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
 public class NavbarWebTest {
 
     @Autowired
