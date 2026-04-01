7607d920dfe38b8e7a4f72fd27e95c3e9c026de4
Ainhoa-Rodriguez <ainhoa.rodriguez@goumh.umh.es>
2026-04-01 15:22:33 +0200

    Usuario controller modified to add protection feature
    Only the admin user can access to the list and description of registered users

--- src/main/java/todolist/controller/UsuarioController.java
+++ src/main/java/todolist/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import todolist.authentication.ManagerUserSession;
+import todolist.controller.exception.UsuarioNoAutorizadoException;
 import todolist.dto.UsuarioData;
 import todolist.service.UsuarioService;
 
@@ -24,7 +25,14 @@ public class UsuarioController {
     public String listadoUsuarios(Model model) {
         // Se obtiene el usuario logueado para la navbar
         Long idUsuarioLogeado = managerUserSession.usuarioLogeado(); // Devuelve el id del usuario que está logueado
+        if (idUsuarioLogeado == null) {
+            return "redirect:/login";
+        }
+
         UsuarioData usuarioLogeado = usuarioService.findById(idUsuarioLogeado); // Buscar datos completos del usuario en base de datos
+        if (usuarioLogeado.getAdmin() == null || !usuarioLogeado.getAdmin()) {
+            throw new UsuarioNoAutorizadoException();
+        }
         model.addAttribute("usuario", usuarioLogeado);
 
         // Se obtiene la lista de todos los usuarios
@@ -39,16 +47,22 @@ public class UsuarioController {
     public String descripcionUsuario(@PathVariable(value = "id") Long idUsuario, Model model) {
         // Se obtiene el usuario logueado para la navbar
         Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (idUsuarioLogeado == null) {
+            return "redirect:/login";
+        }
+
+        // Comprobar que el usuario logueado es admin
         UsuarioData usuarioLogeado = usuarioService.findById(idUsuarioLogeado);
+        if (usuarioLogeado.getAdmin() == null || !usuarioLogeado.getAdmin()) {
+            throw new UsuarioNoAutorizadoException();
+        }
         model.addAttribute("usuario", usuarioLogeado);
 
         // Se obtiene el usuario que se quiere ver en la descripción
         UsuarioData usuarioDescripcion = usuarioService.findById(idUsuario);
-
         if (usuarioDescripcion == null) {
-            throw new RuntimeException("Usurario no encontrado: " + idUsuario);
+            throw new RuntimeException("Usuario no encontrado: " + idUsuario);
         }
-
         model.addAttribute("usuarioDescripcion", usuarioDescripcion);
 
         return "descripcionUsuario";
